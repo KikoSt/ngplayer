0d1da870deb431e6d2eccf3ae292d805fe9b9a2d
Kiko <kiko@webspline.de>
2019-09-29 18:41:17 +0200

    update css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,5 +18,9 @@ export class AppComponent {
   }
 
   public downloadPdf(pdfId: number) {
+      console.log('pdfId = %o', pdfId);
+      const doc = new JsPDF();
+      doc.text(20, 20, 'Hello ' + this.user.username);
+      doc.save('Test.pdf');
   }
 }
